98d9d5600a19a9e60b2103ec5f434988784bd01d
Sofiia Volokha <sofiia.v2008@gmail.com>
2025-11-30 15:39:02 +0200

    Disable Turbopack completely

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,21 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  webpack: (config) => {
+  experimental: {
+    turbopack: false, // полностью отключаем Turbopack
+  },
+
+  webpack(config) {
     config.resolve = config.resolve || {};
     config.resolve.fallback = {
+      ...config.resolve.fallback,
       fs: false,
       path: false,
       crypto: false,
     };
+
     return config;
   },
 };
 
 export default nextConfig;
-
