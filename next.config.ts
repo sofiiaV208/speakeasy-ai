1ee8118b4902c7686319dfae55f9f773d9b7409f
Sofiia Volokha <sofiia.v2008@gmail.com>
2025-11-30 15:44:56 +0200

    Fix next.config for Vercel

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    turbopack: false, // полностью отключаем Turbopack
-  },
-
   webpack(config) {
     config.resolve = config.resolve || {};
     config.resolve.fallback = {
